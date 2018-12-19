85ca4749ae1b827b56583b5dde379f16322b5dea
kamyab <kamyabv@webelight.com>
2018-12-19 14:38:25 +0530

    Added the sorted code

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,45 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableNativeFeedback, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
-const listItems = (props) => {
-   return (
-   <TouchableNativeFeedback onPress={props.onItemPressed}>
-   
-       <View style={styles.listItem} >
-        <Image 
-            resizeMode= "cover" 
-            source={props.placeImage} 
-            style={styles.placeImage} 
-        />
-        <Text>{props.placeName}</Text>
-        </View>
-   </TouchableNativeFeedback>
-   );
-};
+const listItem = props => (
+  <TouchableOpacity onPress={props.onItemPressed}>
+    <View style={styles.listItem}>
+      <Image resizeMode="cover" source={props.placeImage} style={styles.placeImage} />
+      <Text>{props.placeName}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
-    listItem: {
-        width: "100%",
-        padding: 10,
-        backgroundColor: '#eee',
-        marginBottom: 5,
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    placeImage: {
-        marginRight: 8,
-        height: 30,
-        width: 30,
-        borderWidth:1,
-        borderColor:'rgba(0,0,0,0.2)',
-        alignItems:'center',
-        justifyContent:'center',
-        backgroundColor:'#fff',
-        borderRadius:100
-    },
-    allSide: {
-        // borderWidth: 2
-    }
+  listItem: {
+    width: "100%",
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: "#eee",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  placeImage: {
+      marginRight: 8,
+      height: 30,
+      width: 30
+  }
 });
 
-export default listItems;
+export default listItem;
